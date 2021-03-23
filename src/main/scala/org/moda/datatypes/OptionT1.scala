0f8780e1ee044be820dcfb50839e7e3af05651ed
yangxuan_321 <yangx@chinamobiad.com>
2021-03-23 19:52:04 +0800

    scallydb 增加一个字段

--- src/main/scala/org/moda/datatypes/OptionT1.scala
+++ src/main/scala/org/moda/datatypes/OptionT1.scala
@@ -31,8 +31,9 @@ object OptionT1 extends App {
 
     // 如果 使用 OptionT
     // val c = v.map((c: String) => c)
-    val _: OptionT[Future, String] = for {
-      xx <- OptionT(customGreeting(Option.empty[String]))
+    val c = for {
+      // xx <- OptionT(customGreeting(Option.empty[String]))
+      xx <- OptionT(customGreeting(Option("123")))
       _ = println("test3 -> 到这里1")
       xx1 <- OptionT.liftF(Future.successful(xx))
       _ = println("test3 -> 到这里2")
@@ -42,6 +43,22 @@ object OptionT1 extends App {
       _ = println("test3 -> 到这里4")
       _ = println("xx3的值 -> {}", xx3)
     } yield xx3
+    println(c.getOrElse("青萍剑客"))
+  }
+
+  def test4: Unit = {
+    val defaultGreeting: Future[String] = Future.successful("hello, there")
+
+    val greeting: Future[String] = customGreeting(Some("123")).flatMap(custom =>
+      custom.map(Future.successful).getOrElse(defaultGreeting))
+
+  }
+
+  def test5: Unit = {
+    val xc = Option(Future.successful("123"))
+    val xc1 = Option(Future.successful("123"))
+    xc.map(c => c.map(_.concat("123")))
+    xc1.map(c => c.map(_.concat("456")))
   }
 
   def customGreeting(s: Option[String]): Future[Option[String]] ={
